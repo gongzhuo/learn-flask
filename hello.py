7d78cafe0b2974d652b45624b0788daf0777b4eb
gongzhuo <1296634240@qq.com>
2019-10-31 15:43:31 +0800

    git checkout 3a：Flask-Bootstrap 集成 Bootstrap

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 # 渲染模板
 @app.route('/')
@@ -9,3 +12,4 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
