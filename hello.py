79af011ff38d95484a52730a166217b86cbf85b1
gongzhuo <1296634240@qq.com>
2019-11-01 15:43:38 +0800

    git checkout 5a：Flask-Momen设置本地化时间和日期 -- 无法获取本地日期与时间

--- hello.py
+++ hello.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 # 渲染模板
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -16,7 +19,6 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 @app.errorhandler(500)
 def internal_server_error(e):
